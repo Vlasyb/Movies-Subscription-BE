cca551615cab200c45857f9b2925bbd98d2a176c
Vlasyb <vlasyb@gmail.com>
2023-06-24 13:59:58 +0300

    added subscribe,get subscriptions

--- BLL/subscriptionsBLL.js
+++ BLL/subscriptionsBLL.js
@@ -10,7 +10,41 @@ const getSubscriptionById = (id) => {
 	return Subscription.findOne({ _id: id })
 }
 
+const subscribeToMovie = async (memberId, movieId, date) => {
+	const subscription = await Subscription.findOne({
+		memberId: memberId,
+		"movies.movieId": movieId,
+	})
+	console.log(subscription)
+	if (subscription != null) {
+		return "Subscription already exists"
+	}
+	const nonStrDate = stringDateToTypeDate(date)
+	await Subscription.findOneAndUpdate(
+		{ memberId: memberId, "movies.movieId": { $ne: movieId } },
+		{ $addToSet: { movies: { movieId: movieId, date: nonStrDate } } },
+		{ upsert: true } //if it doesnt exist creates new
+	)
+	return "Member Subscribed"
+}
+
+const getSubscriptionsForMember = async (memberId) => {
+	const subscriptions = await Subscription.find({ memberId: memberId })
+	return subscriptions
+}
+
+const stringDateToTypeDate = (dateString) => {
+	const parts = dateString.split("/")
+	const day = parseInt(parts[0], 10) + 1 // maybe need to remove the +1
+	const month = parseInt(parts[1], 10) - 1 // Extract month as integer (subtract 1 as months are zero-based in JavaScript)
+	const year = parseInt(parts[2], 10)
+
+	return new Date(year, month, day) // Create Date object
+}
+
 module.exports = {
+	getSubscriptionsForMember,
+	subscribeToMovie,
 	getAllSubscriptions,
 	getSubscriptionById,
 }
--- routes/subscriptionsRouter.js
+++ routes/subscriptionsRouter.js
@@ -20,7 +20,32 @@ router.get("/subscription/:id", async (req, res) => {
 		res.status(200).json(subscription)
 	} catch (error) {
 		console.log("error ", error)
-		res.status.send(error.message)
+		res.status(500).send(error.message)
+	}
+})
+//get all subscriptions for memberId
+router.get("/subscriptions/:memberId", async (req, res) => {
+	try {
+		const { memberId } = req.params
+		const subscriptions = await subscriptionsBLL.getSubscriptionsForMember(
+			memberId
+		)
+		res.status(200).json(subscriptions)
+	} catch (error) {
+		console.log("error ", error)
+		res.status(500).send(error.message)
+	}
+})
+
+router.put("/subscribe/:movieId", async (req, res) => {
+	try {
+		const { movieId } = req.params
+		const { memberId, date } = req.body
+		result = await subscriptionsBLL.subscribeToMovie(memberId, movieId, date)
+		res.status(201).json(result)
+	} catch (error) {
+		console.log("error ", error)
+		res.status(500).send(error.message)
 	}
 })
 
